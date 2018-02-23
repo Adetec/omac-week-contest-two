efbfb2c3500a6b6aaaabbe9a2ace37c6654e5133
Adel Lassag <adetech.aadl@gmail.com>
2018-02-23 02:58:40 +0100

    add some animation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-// changer h1 à h6 en majiscule
-// function majiscule() {
+// changer h1 à h6 en majiscule...
+
 function majiscule() {
 	var arr = ['h1','h2','h3','h4','h5','h6'], maj, i;
 	for (i = 0 ; i < arr.length; i++) {
@@ -8,8 +8,10 @@ function majiscule() {
 	}
 
 }
-
-window.onscroll = function() {scrollProg()};
+// parametrer  scroll progression...
+window.onscroll = function() {
+	scrollProg();
+};
 
 function scrollProg() {
   var scroll = document.body.scrollTop || document.documentElement.scrollTop;
@@ -17,3 +19,13 @@ function scrollProg() {
   var scrolled = (scroll / height) * 100;
   document.getElementById("scrollPr").style.width = scrolled + "%";
 }
+
+// charger la page progressievement...
+function fade(){
+	document.body.className += 'fadeIn';
+	console.log("it's faded");	
+}
+
+
+
+
